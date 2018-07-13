ea63c106b6ba8cf796c86931903056242981fa1b
anjalisebastian <anjalisebastian@icloud.com>
2018-07-13 18:17:46 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -16,7 +16,7 @@ class GardenGnome
  end
  
  def gnaw
-   puts "Gnawing on a tree!!!"
+   "Gnawing on a tree!!!"
  end
  
  def shout
@@ -27,5 +27,5 @@ class GardenGnome
    puts "Hello humans my name is Robert, I am 23 years old, and you'll rue the day you crossed me!"
  end
  
- gnaw
+ puts gnaw
  end 
